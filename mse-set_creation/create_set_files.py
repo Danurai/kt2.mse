72e7ed3243379b7a7e8a46f4a6e9b41181579167
Danurai <bikerdan13@yahoo.co.uk>
2021-09-25 19:01:34 +0100

    set creation card_list_column set to 0dp

--- mse-set_creation/create_set_files.py
+++ mse-set_creation/create_set_files.py
@@ -5,6 +5,7 @@ import numpy
 outfile='./kt2cards.mse-game/card_fields.mse'
 
 df = pd.read_excel('./mse-set_creation/kt2cards.xlsx', 'card_fields')
+
 f = open(outfile,'w')
 
 for row in df.itertuples():
@@ -15,10 +16,10 @@ for row in df.itertuples():
 			if key != 'Index':
 				if key == 'choices':
 					for choice in val.split(','):
-						f.write('\tchoice:\t' + str(choice) + '\n')
+						f.write(f'\tchoice:\t{str(choice)}\n')
 				else:
-					def b(x): return str(x).lower() if isinstance(x, bool) else str(x)
-					f.write('\t' + key + ':\t' + b(val) + '\n')
+					def b(x): return str(x).lower() if isinstance(x, bool) else str(x).replace('.0','')
+					f.write(f'\t{key}:\t{b(val)}\n')
 	f.write('\n')
 
 f.close()
